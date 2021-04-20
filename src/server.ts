da1eda1ce3fc0d1821e380a51a33d233aae63449
LucianoPierdona <lucianompj1@gmail.com>
2021-04-20 10:30:31 -0300

    created create settings route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import './database';
+import { routes } from './routes';
 
 const app = express();
+
+app.use(routes);
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
