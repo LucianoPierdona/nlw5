57d44d0db48f5ebe9767c62eac1ae1a34f3eb4d5
LucianoPierdona <lucianompj1@gmail.com>
2021-04-21 20:52:08 -0300

    created settingsservice

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -6,15 +6,6 @@ export class SettingsController {
   async create(req: Request, res: Response) {
     const { chat, username } = req.body;
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-      chat,
-      username,
-    });
-
-    await settingsRepository.save(settings);
-
-    return res.json(settings);
+    return res.json('');
   }
 }
