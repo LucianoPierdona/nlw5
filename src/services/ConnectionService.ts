83e4efa7d9d56d5a44a9a7fedfed298c0d3ce42b
LucianoPierdona <lucianompj1@gmail.com>
2021-04-22 20:42:45 -0300

    created create connection function

--- src/services/ConnectionService.ts
+++ src/services/ConnectionService.ts
@@ -2,10 +2,32 @@ import { getCustomRepository, Repository } from 'typeorm';
 import { ConnectionRepository } from '../repositories/ConnectionRepository';
 import { Connection } from '../entities/Connection';
 
+interface ICreateConnection {
+  socket_id: string;
+  user_id: string;
+  admin_id?: string;
+  id?: string;
+}
+
 export class ConnectionService {
   private connectionRepository: Repository<Connection>;
 
   constructor() {
     this.connectionRepository = getCustomRepository(ConnectionRepository);
+  }
+
+  async create(data: ICreateConnection) {
+    const { socket_id, admin_id, user_id, id } = data;
+
+    const connection = this.connectionRepository.create({
+      socket_id,
+      admin_id,
+      id,
+      user_id,
+    });
+
+    await this.connectionRepository.save(connection);
+
+    return connection;
   }
 }
