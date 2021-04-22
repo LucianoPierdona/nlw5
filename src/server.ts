2623be7265b120ab8b5d8bc8a6d5b34cd219334a
LucianoPierdona <lucianompj1@gmail.com>
2021-04-22 20:16:12 -0300

    added public files

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
 import express from 'express';
 import './database';
 import { routes } from './routes';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on('connection', (socket: Socket) => {
+  console.log('se conectou', socket.id);
+});
 
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
