30df6fcc0de94069cd839120ff8b022993474f27
LucianoPierdona <lucianompj1@gmail.com>
2021-04-21 21:16:38 -0300

    created messageService

---
+++ src/services/MessageService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from 'typeorm';
+import { MessageRepository } from '../repositories/MessageRepository';
+import { UserRepository } from '../repositories/UserRepository';
+
+interface ICreateMessage {
+  admin_id: string;
+  text: string;
+  user_id: string;
+}
+
+export class MessageService {
+  async create(data: ICreateMessage) {
+    const { admin_id, text, user_id } = data;
+    const messageRepository = getCustomRepository(MessageRepository);
+
+    const message = messageRepository.create({
+      admin_id,
+      text,
+      user_id,
+    });
+
+    await messageRepository.save(message);
+
+    return message;
+  }
+}
